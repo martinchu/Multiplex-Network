62943f3c8060518109cbb796d273cdc03cb470a7
Martin Chu <martinchu09@gmail.com>
2017-03-14 22:35:25 +0000

    update model

--- model.py
+++ model.py
@@ -1,8 +1,12 @@
 class Bank(object):
-
+    BankTotal = 0;
     def __init__(self, c, mc):
         self.capital = c
         self.minCapital = mc
+        BankTotal++;
 
     def print_capital(self):
         print "Bank Capital: {} \nMinimum Capital: {} ".format(self.capital, self.minCapital)
+
+
+    def updateCapital(self,exposure):
